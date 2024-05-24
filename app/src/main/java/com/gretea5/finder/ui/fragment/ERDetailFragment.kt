72b4e9a260caccf401baa2191d6bf7ebae8a1ee9
gretea5 <gretea4489@gmail.com>
2024-05-24 18:20:07 +0900

    Design: Toolbar 생성 및 응급실 이름 보여지게 하기

--- app/src/main/java/com/gretea5/finder/ui/fragment/ERDetailFragment.kt
+++ app/src/main/java/com/gretea5/finder/ui/fragment/ERDetailFragment.kt
@@ -1,15 +1,20 @@
 package com.gretea5.finder.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.gretea5.finder.databinding.FragmentErDetailBinding
+import com.gretea5.finder.ui.viewmodel.ERViewModel
 
 class ERDetailFragment : Fragment() {
     private var _binding : FragmentErDetailBinding? = null
     private val binding get() = _binding!!
+
+    private val erViewModel : ERViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,15 @@ class ERDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentErDetailBinding.inflate(inflater)
+
+        var name = erViewModel.erDetailData.value?.name
+
+        if (name?.length!! >= 15) {
+            name = "${name.substring(0, 16)}..."
+        }
+
+        binding.erName.text = name
+
         return binding.root
     }
 }
